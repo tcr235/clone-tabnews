e742aa7158cb2e311c3bad5589b7b61abe3897a2
tcr235 <thiagoconterocha@gmail.com>
2025-12-01 14:31:33 -0300

    modified message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Se você leu isso, beba água e me mande uma mensagem dizendo "acabei de beber"</h1>
+    return <h1>Se você leu isso, beba água e me mande uma mensagem dizendo "acabei de beber".</h1>
 }
 
 export default Home;
