8bffca35d80f0126b4ff87e1c320a6aa6f0607dd
tcr235 <thiagoconterocha@gmail.com>
2025-12-02 14:42:17 -0300

    added `lint:check` and `lint:fix` scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto de estudos com o intuito de aprender, de ponta a ponta, a construção de um projeto real.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Se você leu isso, beba água e me mande uma mensagem dizendo "acabei de beber".</h1>
+function Home() {
+  return (
+    <h1>
+      Se você leu isso, beba água e me mande uma mensagem dizendo "acabei de
+      beber".
+    </h1>
+  );
 }
 
 export default Home;
